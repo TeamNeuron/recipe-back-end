5ead43619e9b0c08293fc810e6b82e705c551e1a
Eric Hebert <ayebear@gmail.com>
2015-12-01 12:34:20 -0500

    Committing Zain's and Hamza's front-end code

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById("foot01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='recipe.html'>Home</a></li>" +
+
+/* "<li><a href='https://www.google.com'>Google</a></li>" + */
+
+"</ul>";
