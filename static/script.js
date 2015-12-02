8a3a84772a27ed467dccb9a053512050de0933f0
hamzasaeed12 <hsaeed1@umbc.edu>
2015-12-01 19:02:50 -0500

    Added better front end

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='recipe.html'>Home</a></li>" +
-
-/* "<li><a href='https://www.google.com'>Google</a></li>" + */
-
-"</ul>";
